09195893c5c01f2cf69f751fc5c4916bb7edd90b
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-11-11 17:23:25 -0300

    Add files via upload

---
+++ comentario.js
@@ -0,0 +1,14 @@
+
+console.log("Passo 001") 
+console.log("Passo 003")
+//console.log("Passo #02")
+
+/*
+   *Esse é um 
+   *comentário 
+   *de múltiplas
+   *linhas!
+*/
+
+console.log("Passo 002")
+
---
+++ sentenca02.js
@@ -0,0 +1,14 @@
+console.log(1+3)
+console.log("Cool")
+
+//this order is important
+
+
+console.log("Passo 001")
+console.log("Passo 002")
+console.log("Passo 003")
+
+
+
+
+
