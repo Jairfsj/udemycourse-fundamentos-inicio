1859ee77921b00c7e3aae91b7709f567ba596f62
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-11-11 18:06:55 -0300

    Add files via upload

---
+++ bloco.js
@@ -0,0 +1,14 @@
+{
+   console.log("Passo 001")
+   console.log("Passo 002")
+   console.log("Passo 003")
+}
+
+{
+   console.log("Passo 004")
+   console.log("Passo 005")
+}
+
+{
+   console.log("fim;")
+}
