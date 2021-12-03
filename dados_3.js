3dd052d509f3412f0a38b79f611a7eaf536d130d
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-12-02 22:54:13 -0300

    Add files via upload

---
+++ dados_3.js
@@ -0,0 +1,13 @@
+console.log(19.9 * 0.6);
+
+let price = 19.9;
+let out = 0.4;
+let priceWithOut = price * (1 - out);
+console.log(priceWithOut);
+
+let name = "Notebook";
+let category = "PaperShop";
+console.log("Product: "  + name
+   +", Category: " +  category 
+   +", Price :"  + price
+   +", Out: "  + out);
