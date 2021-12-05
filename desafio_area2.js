186ac8fd6e41f15b462ce86db4a4a2405d8fbd51
Jair Francisco da Silva Junior <61885860+Jairfsj@users.noreply.github.com>
2021-12-05 16:55:53 -0300

    Add files via upload

---
+++ desafio_area2.js
@@ -0,0 +1,8 @@
+const PI = 3.141592;
+const raio =  14;
+let  areaCircuf = PI * raio * raio;
+
+console.log("O valor da área é "  + areaCircuf + "m2.");
+
+areaCircuf = Math.PI * raio * raio;
+console.log("O valor da área é " + areaCircuf + "m2.");
